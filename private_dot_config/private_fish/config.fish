cbd1e8ee26f5837a28bfac2499649be41e27c8d5
Mike Delaney <mdelaney@utexas.edu>
2023-03-05 09:53:04 -0800

    Fix XPS config for nvim, add config.fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source ~/.asdf/asdf.fish
+
+# May need set -x here
+set BROWSER /usr/bin/firefox
+set -gx EDITOR /home/miked/Applications/nvim.appimage
+
+alias ls exa
+alias vim /home/miked/Applications/nvim.appimage
+
+direnv hook fish | source
+
+# Created by `pipx` on 2022-09-24 21:11:55
+set PATH $PATH /home/miked/.local/bin
