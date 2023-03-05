03a23c73728f60761009c8b8d1569faa6d2f9ff3
Mike Delaney <mdelaney@utexas.edu>
2023-03-05 14:49:12 -0800

    More fixes for XPS config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,11 +6,12 @@ source ~/.asdf/asdf.fish
 
 # May need set -x here
 set BROWSER /usr/bin/firefox
-set -gx EDITOR /home/miked/Applications/nvim.appimage
+set -gx EDITOR nvim
 set -gx PATH "$HOME/.cargo/bin" $PATH
 
 alias ls exa
-alias vim /home/miked/Applications/nvim.appimage
+alias vim nvim
+alias vi nvim
 
 direnv hook fish | source
 
