afef2879bed9eb12ca87ac996711cc8073f7e332
Mike Delaney <mdelaney@utexas.edu>
2023-03-05 13:20:52 -0800

    Add cargo to PATH

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,6 +7,7 @@ source ~/.asdf/asdf.fish
 # May need set -x here
 set BROWSER /usr/bin/firefox
 set -gx EDITOR /home/miked/Applications/nvim.appimage
+set -gx PATH "$HOME/.cargo/bin" $PATH
 
 alias ls exa
 alias vim /home/miked/Applications/nvim.appimage
